009eb1930798ede53e312620d6d7d42e4c1c9216
deyvidandradees <deyvid-asilva@hotmail.com>
2022-09-11 20:44:23 -0300

    * Alterações na main

--- app/src/main/java/com/dws/oxie/activities/MainActivity.kt
+++ app/src/main/java/com/dws/oxie/activities/MainActivity.kt
@@ -7,10 +7,7 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.dws.oxie.R
 import com.dws.oxie.assistentes.AnimacaoBotao
@@ -28,12 +25,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var progressOxigenacao: CircularProgressIndicator
     private lateinit var progressPassos: CircularProgressIndicator
 
-    private var arrayAtualizacoes = ArrayList<Double>()
+    private var arrayAtualizacoes = arrayListOf(1, 1, 1)
 
     private lateinit var mainHandler: Handler
+
     private val updateTextTask = object : Runnable {
         override fun run() {
-            fazerRequisicao(true)
+            arrayAtualizacoes.clear()
+            arrayAtualizacoes = fazerRequisicaoRotas()
+            Log.d("DWS.D", arrayAtualizacoes.toString())
+
+            updateUI(arrayAtualizacoes)
+
             mainHandler.postDelayed(this, 1000)
         }
     }
@@ -53,6 +56,7 @@ class MainActivity : AppCompatActivity() {
 
         val btnConectar = findViewById<RelativeLayout>(R.id.btn_conectar)
         val btnDesconectar = findViewById<RelativeLayout>(R.id.btn_desconectar)
+        val btnMenu = findViewById<ImageView>(R.id.btn_menu)
 
         val btnHistoricoBatimentos = findViewById<LinearLayout>(R.id.li_historico_batimentos)
         val btnHistoricoOxigenacao = findViewById<LinearLayout>(R.id.li_historico_oxigenacao)
@@ -73,6 +77,12 @@ class MainActivity : AppCompatActivity() {
 
             btnDesconectar.visibility = View.GONE
             btnConectar.visibility = View.VISIBLE
+        }
+        btnMenu.setOnClickListener { view ->
+            AnimacaoBotao.animar(view)
+
+            val intent = Intent(this, ConfiguracoesActivity::class.java)
+            startActivity(intent)
         }
 
         btnHistoricoBatimentos.setOnClickListener { view ->
@@ -99,6 +109,38 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun fazerRequisicaoRotas(): ArrayList<Int> {
+        val ip = "192.168.0.133"
+        val porta = 80
+        val array = arrayListOf(0, 0, 0)
+
+        val urlConnection = URL("HTTP", ip, porta, "cu").openConnection() as URLConnection
+
+        val thread = Thread {
+            try {
+                val res = urlConnection.inputStream.bufferedReader().readText().split(", ")
+                array.clear()
+                array.add(res[2].toInt())
+                array.add(res[1].toInt())
+                array.add(res[0].toInt())
+
+            } catch (e: NoRouteToHostException) {
+               // Toast.makeText(
+               //     application.applicationContext,
+               //     "Falha ao conectar com o dispositivo.",
+               //     Toast.LENGTH_LONG
+               // )
+               //     .show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        thread.start()
+        thread.join(500)
+        return array
+    }
+
     private fun fazerRequisicao(debug: Boolean) {
         val ip = if (debug) "192.168.0.175" else "192.168.0.133"
         val porta = if (debug) 5000 else 80
@@ -110,13 +152,17 @@ class MainActivity : AppCompatActivity() {
             try {
                 val text = urlConnection.inputStream.bufferedReader().readText()
 
+                val batimento = text.toInt()
+                val oxigenacao = text.toInt()
+                val passos = text.toInt()
+
                 val regex = Regex("<span id=\"(.+?)\">(.*?)</span>")
                 val matches = regex.findAll(text)
 
                 arrayAtualizacoes.clear()
 
                 matches.iterator().forEach { item ->
-                    arrayAtualizacoes.add(item.groups[2]!!.value.toDouble())
+                    arrayAtualizacoes.add(item.groups[2]!!.value.toInt())
                 }
 
             } catch (e: NoRouteToHostException) {
@@ -134,31 +180,34 @@ class MainActivity : AppCompatActivity() {
         thread.start()
         thread.join()
 
-        updateUI(arrayAtualizacoes)
+        //updateUI(arrayAtualizacoes)
     }
 
     @SuppressLint("SetTextI18n")
-    fun updateUI(arrayValores: ArrayList<Double>) {
+    fun updateUI(arrayValores: ArrayList<Int>) {
         Log.d("DWS.D", arrayValores.toString())
 
-        textBatimentos.text = "${arrayValores[0].toInt()}/80"
-        textOxigenacao.text = "${arrayValores[1].toInt()}%"
-        textPassos.text = "${arrayValores[2].toInt()}/6000"
+        textBatimentos.text =
+            if (arrayValores[0] > 0) "${arrayValores[0]}BPM" else textBatimentos.text
 
-        progressBatimentos.progress = ((arrayValores[0] * 100) / 150).toInt()
-        progressOxigenacao.progress = ((arrayValores[1] * 100) / 100).toInt()
-        progressPassos.progress = ((arrayValores[2] * 100) / 6000).toInt()
+        textOxigenacao.text =
+            if (arrayValores[1] > 0) "${arrayValores[1]}%" else textOxigenacao.text
+
+        textPassos.text =
+            if (arrayValores[2] > 0) "${arrayValores[2]}/6000" else textPassos.text
+
+        progressBatimentos.progress = (
+                (if (arrayValores[0] > 0) arrayValores[0] else progressBatimentos.progress) * 100
+                ) / 110
+
+        progressOxigenacao.progress = (
+                (if (arrayValores[1] > 0) arrayValores[1] else progressOxigenacao.progress) * 100
+                ) / 100
+
+        progressPassos.progress = (
+                (if (arrayValores[2] > 0) arrayValores[2] else progressPassos.progress) * 100
+                ) / 6000
     }
-
-    /*override fun onPause() {
-        super.onPause()
-        mainHandler.removeCallbacks(updateTextTask)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mainHandler.post(updateTextTask)
-    }*/
 
     override fun onDestroy() {
         super.onDestroy()
