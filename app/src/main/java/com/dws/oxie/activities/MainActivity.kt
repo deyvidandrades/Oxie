1d104ca4eb1e9ba9d368af67fc63a912eab1a6ce
deyvidandradees <deyvid-asilva@hotmail.com>
2022-09-11 13:08:44 -0300

    * update de classe

--- app/src/main/java/com/dws/oxie/activities/MainActivity.kt
+++ app/src/main/java/com/dws/oxie/activities/MainActivity.kt
@@ -1,12 +1,18 @@
-package com.dws.oxie
+package com.dws.oxie.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.dws.oxie.R
 import com.dws.oxie.assistentes.AnimacaoBotao
 import com.google.android.material.progressindicator.CircularProgressIndicator
 import java.net.NoRouteToHostException
@@ -24,6 +30,14 @@ class MainActivity : AppCompatActivity() {
 
     private var arrayAtualizacoes = ArrayList<Double>()
 
+    private lateinit var mainHandler: Handler
+    private val updateTextTask = object : Runnable {
+        override fun run() {
+            fazerRequisicao(true)
+            mainHandler.postDelayed(this, 1000)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -38,15 +52,54 @@ class MainActivity : AppCompatActivity() {
         progressPassos = findViewById(R.id.progress_passos)
 
         val btnConectar = findViewById<RelativeLayout>(R.id.btn_conectar)
+        val btnDesconectar = findViewById<RelativeLayout>(R.id.btn_desconectar)
+
+        val btnHistoricoBatimentos = findViewById<LinearLayout>(R.id.li_historico_batimentos)
+        val btnHistoricoOxigenacao = findViewById<LinearLayout>(R.id.li_historico_oxigenacao)
+        val btnHistoricoPassos = findViewById<LinearLayout>(R.id.li_historico_passos)
+
+        mainHandler = Handler(Looper.getMainLooper())
 
         btnConectar.setOnClickListener { view ->
             AnimacaoBotao.animar(view)
-            fazerRequisicao(true)
+            mainHandler.post(updateTextTask)
+
+            btnConectar.visibility = View.GONE
+            btnDesconectar.visibility = View.VISIBLE
         }
 
+        btnDesconectar.setOnClickListener { view ->
+            mainHandler.removeCallbacks(updateTextTask)
+
+            btnDesconectar.visibility = View.GONE
+            btnConectar.visibility = View.VISIBLE
+        }
+
+        btnHistoricoBatimentos.setOnClickListener { view ->
+            AnimacaoBotao.animar(view)
+
+            val intent = Intent(this, HistoricoActivity::class.java)
+            intent.putExtra("tipo", 0)
+            startActivity(intent)
+
+        }
+        btnHistoricoOxigenacao.setOnClickListener { view ->
+            AnimacaoBotao.animar(view)
+            val intent = Intent(this, HistoricoActivity::class.java)
+            intent.putExtra("tipo", 1)
+            startActivity(intent)
+
+        }
+        btnHistoricoPassos.setOnClickListener { view ->
+            AnimacaoBotao.animar(view)
+            val intent = Intent(this, HistoricoActivity::class.java)
+            intent.putExtra("tipo", 2)
+            startActivity(intent)
+
+        }
     }
 
-    fun fazerRequisicao(debug: Boolean) {
+    private fun fazerRequisicao(debug: Boolean) {
         val ip = if (debug) "192.168.0.175" else "192.168.0.133"
         val porta = if (debug) 5000 else 80
 
@@ -95,5 +148,20 @@ class MainActivity : AppCompatActivity() {
         progressBatimentos.progress = ((arrayValores[0] * 100) / 150).toInt()
         progressOxigenacao.progress = ((arrayValores[1] * 100) / 100).toInt()
         progressPassos.progress = ((arrayValores[2] * 100) / 6000).toInt()
+    }
+
+    /*override fun onPause() {
+        super.onPause()
+        mainHandler.removeCallbacks(updateTextTask)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mainHandler.post(updateTextTask)
+    }*/
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mainHandler.removeCallbacks(updateTextTask)
     }
 }
