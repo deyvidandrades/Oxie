a0821695b3dcdf4c823efaed39f682b33180f386
deyvidandradees <deyvid-asilva@hotmail.com>
2022-09-10 22:45:56 -0300

    init

---
+++ app/src/main/java/com/dws/oxie/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.dws.oxie
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.dws.oxie.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+}
