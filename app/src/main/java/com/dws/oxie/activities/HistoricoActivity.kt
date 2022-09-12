9afbf164efe4c8c80f8c141657e36ca46c60a970
deyvidandradees <deyvid-asilva@hotmail.com>
2022-09-12 19:06:06 -0300

    * updadte HistoricoActivity.kt.kt

--- app/src/main/java/com/dws/oxie/activities/HistoricoActivity.kt
+++ app/src/main/java/com/dws/oxie/activities/HistoricoActivity.kt
@@ -1,19 +1,16 @@
 package com.dws.oxie.activities
 
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.dws.mufla.adaptadores.AdaptadorItemHistorico
 import com.dws.oxie.R
+import com.dws.oxie.adaptadores.AdaptadorItemHistorico
 import com.dws.oxie.assistentes.AnimacaoBotao
 import com.dws.oxie.objetos.ItemHistorico
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.util.*
+import com.dws.oxie.persistencia.Persistencia
 
 class HistoricoActivity : AppCompatActivity() {
     private var arrayItemHistorico = ArrayList<ItemHistorico>()
@@ -25,11 +22,6 @@ class HistoricoActivity : AppCompatActivity() {
 
         val recyclerHistorico = findViewById<RecyclerView>(R.id.recycler_historico)
         val btnVoltar = findViewById<ImageView>(R.id.btn_voltar)
-        val liBatimentos = findViewById<LinearLayout>(R.id.li_batimentos)
-        val liOxigenacao = findViewById<LinearLayout>(R.id.li_oxigenacao)
-        val liPassos = findViewById<LinearLayout>(R.id.li_passos)
-
-        val tipo: Int = intent.getIntExtra("tipo", 0)
 
         val layoutManagerHorarioOnibus =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -39,25 +31,6 @@ class HistoricoActivity : AppCompatActivity() {
         recyclerHistorico.adapter = adaptadorHorarioOnibus
         recyclerHistorico.layoutManager = layoutManagerHorarioOnibus
 
-
-        when (tipo) {
-            1 -> {
-                liBatimentos.visibility = View.GONE
-                liOxigenacao.visibility = View.VISIBLE
-                liPassos.visibility = View.GONE
-            }
-            2 -> {
-                liBatimentos.visibility = View.GONE
-                liOxigenacao.visibility = View.GONE
-                liPassos.visibility = View.VISIBLE
-            }
-            else -> {
-                liBatimentos.visibility = View.VISIBLE
-                liOxigenacao.visibility = View.GONE
-                liPassos.visibility = View.GONE
-            }
-        }
-
         btnVoltar.setOnClickListener { view ->
             AnimacaoBotao.animar(view)
             finish()
@@ -66,16 +39,10 @@ class HistoricoActivity : AppCompatActivity() {
         carregarHistorico()
     }
 
-
     private fun carregarHistorico() {
-        val calendario = Calendar.getInstance()
-        val df: DateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
-        val data: String = df.format(calendario.time)
+        val persistencia = Persistencia(applicationContext)
+        val dados = persistencia.carregarDados("historico")
 
-        arrayItemHistorico.add(ItemHistorico(data, 87.0, "bpm"))
-        arrayItemHistorico.add(ItemHistorico(data, 74.0, "bpm"))
-        arrayItemHistorico.add(ItemHistorico(data, 68.0, "bpm"))
-        arrayItemHistorico.add(ItemHistorico(data, 120.0, "bpm"))
-        arrayItemHistorico.add(ItemHistorico(data, 62.0, "bpm"))
+        arrayItemHistorico.addAll(dados)
     }
 }
