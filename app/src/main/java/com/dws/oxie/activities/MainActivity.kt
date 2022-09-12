27b3947fd8f1c6186e501cc5ab00eba8bceb410e
deyvidandradees <deyvid-asilva@hotmail.com>
2022-09-12 19:07:06 -0300

    * updadte MainActivity.kt

--- app/src/main/java/com/dws/oxie/activities/MainActivity.kt
+++ app/src/main/java/com/dws/oxie/activities/MainActivity.kt
@@ -5,16 +5,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.View
-import android.widget.*
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.dws.oxie.R
+import com.dws.oxie.adaptadores.AdaptadorItemHistorico
 import com.dws.oxie.assistentes.AnimacaoBotao
+import com.dws.oxie.objetos.ItemHistorico
+import com.dws.oxie.persistencia.Persistencia
 import com.google.android.material.progressindicator.CircularProgressIndicator
 import java.net.NoRouteToHostException
 import java.net.URL
 import java.net.URLConnection
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textBatimentos: TextView
@@ -25,17 +32,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var progressOxigenacao: CircularProgressIndicator
     private lateinit var progressPassos: CircularProgressIndicator
 
-    private var arrayAtualizacoes = arrayListOf(1, 1, 1)
+    private val debug: Boolean = true
+    private var arrayItemHistorico = ArrayList<ItemHistorico>()
 
     private lateinit var mainHandler: Handler
-
     private val updateTextTask = object : Runnable {
         override fun run() {
-            arrayAtualizacoes.clear()
-            arrayAtualizacoes = fazerRequisicaoRotas()
-            Log.d("DWS.D", arrayAtualizacoes.toString())
+            var arrayAtualizacoes = fazerRequisicao()
+
+            if (arrayAtualizacoes.size == 0)
+                arrayAtualizacoes = arrayListOf(0, 0, 0)
 
             updateUI(arrayAtualizacoes)
+            salvarDados(arrayAtualizacoes)
+            carregarHistorico()
 
             mainHandler.postDelayed(this, 1000)
         }
@@ -54,15 +64,51 @@ class MainActivity : AppCompatActivity() {
         progressOxigenacao = findViewById(R.id.progress_oxigenacao)
         progressPassos = findViewById(R.id.progress_passos)
 
+        mainHandler = Handler(Looper.getMainLooper())
+
+        val recyclerHistorico = findViewById<RecyclerView>(R.id.recycler_historico)
+
+        val layoutManagerHorarioOnibus =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        val adaptadorHorarioOnibus = AdaptadorItemHistorico(this, arrayItemHistorico)
+
+        recyclerHistorico.setHasFixedSize(true)
+        recyclerHistorico.adapter = adaptadorHorarioOnibus
+        recyclerHistorico.layoutManager = layoutManagerHorarioOnibus
+
+        configurarBotoes()
+        carregarHistorico()
+    }
+
+    private fun carregarHistorico() {
+        val persistencia = Persistencia(applicationContext)
+        val dados = persistencia.carregarDados("historico")
+        val reHistorico = findViewById<RelativeLayout>(R.id.re_historico)
+
+        if (dados.size > 0) {
+            reHistorico.visibility = View.VISIBLE
+
+            updateUI(
+                arrayListOf(
+                    dados[0].getValorBatimentos(),
+                    dados[0].getValorOxigenacao(),
+                    (dados[0].getValorPassos() * 6000) / 100
+                )
+            )
+
+            arrayItemHistorico.add(dados[0])
+
+            if (dados.size > 1)
+                arrayItemHistorico.add(dados[1])
+        } else
+            reHistorico.visibility = View.GONE
+    }
+
+    private fun configurarBotoes() {
         val btnConectar = findViewById<RelativeLayout>(R.id.btn_conectar)
         val btnDesconectar = findViewById<RelativeLayout>(R.id.btn_desconectar)
         val btnMenu = findViewById<ImageView>(R.id.btn_menu)
-
-        val btnHistoricoBatimentos = findViewById<LinearLayout>(R.id.li_historico_batimentos)
-        val btnHistoricoOxigenacao = findViewById<LinearLayout>(R.id.li_historico_oxigenacao)
-        val btnHistoricoPassos = findViewById<LinearLayout>(R.id.li_historico_passos)
-
-        mainHandler = Handler(Looper.getMainLooper())
+        val btnVerTodos = findViewById<TextView>(R.id.tv_ver_todos)
 
         btnConectar.setOnClickListener { view ->
             AnimacaoBotao.animar(view)
@@ -85,36 +131,20 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        btnHistoricoBatimentos.setOnClickListener { view ->
-            AnimacaoBotao.animar(view)
-
-            val intent = Intent(this, HistoricoActivity::class.java)
-            intent.putExtra("tipo", 0)
-            startActivity(intent)
-
-        }
-        btnHistoricoOxigenacao.setOnClickListener { view ->
+        btnVerTodos.setOnClickListener { view ->
             AnimacaoBotao.animar(view)
             val intent = Intent(this, HistoricoActivity::class.java)
-            intent.putExtra("tipo", 1)
             startActivity(intent)
-
-        }
-        btnHistoricoPassos.setOnClickListener { view ->
-            AnimacaoBotao.animar(view)
-            val intent = Intent(this, HistoricoActivity::class.java)
-            intent.putExtra("tipo", 2)
-            startActivity(intent)
-
         }
     }
 
-    private fun fazerRequisicaoRotas(): ArrayList<Int> {
-        val ip = "192.168.0.133"
-        val porta = 80
+    private fun fazerRequisicao(): ArrayList<Int> {
+        val ip = if (debug) "192.168.0.175" else "192.168.0.133"
+        val porta = if (debug) 5000 else 80
         val array = arrayListOf(0, 0, 0)
 
-        val urlConnection = URL("HTTP", ip, porta, "cu").openConnection() as URLConnection
+        val urlConnection =
+            URL("HTTP", ip, porta, if (debug) "" else "cu").openConnection() as URLConnection
 
         val thread = Thread {
             try {
@@ -125,12 +155,7 @@ class MainActivity : AppCompatActivity() {
                 array.add(res[0].toInt())
 
             } catch (e: NoRouteToHostException) {
-               // Toast.makeText(
-               //     application.applicationContext,
-               //     "Falha ao conectar com o dispositivo.",
-               //     Toast.LENGTH_LONG
-               // )
-               //     .show()
+
             } catch (e: Exception) {
                 e.printStackTrace()
             }
@@ -141,54 +166,11 @@ class MainActivity : AppCompatActivity() {
         return array
     }
 
-    private fun fazerRequisicao(debug: Boolean) {
-        val ip = if (debug) "192.168.0.175" else "192.168.0.133"
-        val porta = if (debug) 5000 else 80
-
-        val url = URL("HTTP", ip, porta, "")
-        val urlConnection = url.openConnection() as URLConnection
-
-        val thread = Thread {
-            try {
-                val text = urlConnection.inputStream.bufferedReader().readText()
-
-                val batimento = text.toInt()
-                val oxigenacao = text.toInt()
-                val passos = text.toInt()
-
-                val regex = Regex("<span id=\"(.+?)\">(.*?)</span>")
-                val matches = regex.findAll(text)
-
-                arrayAtualizacoes.clear()
-
-                matches.iterator().forEach { item ->
-                    arrayAtualizacoes.add(item.groups[2]!!.value.toInt())
-                }
-
-            } catch (e: NoRouteToHostException) {
-                Toast.makeText(
-                    application.applicationContext,
-                    "Falha ao conectar com o dispositivo.",
-                    Toast.LENGTH_LONG
-                )
-                    .show()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-
-        thread.start()
-        thread.join()
-
-        //updateUI(arrayAtualizacoes)
-    }
-
     @SuppressLint("SetTextI18n")
-    fun updateUI(arrayValores: ArrayList<Int>) {
-        Log.d("DWS.D", arrayValores.toString())
+    private fun updateUI(arrayValores: ArrayList<Int>) {
 
         textBatimentos.text =
-            if (arrayValores[0] > 0) "${arrayValores[0]}BPM" else textBatimentos.text
+            if (arrayValores[0] > 0) "${arrayValores[0]}" else textBatimentos.text
 
         textOxigenacao.text =
             if (arrayValores[1] > 0) "${arrayValores[1]}%" else textOxigenacao.text
@@ -196,17 +178,25 @@ class MainActivity : AppCompatActivity() {
         textPassos.text =
             if (arrayValores[2] > 0) "${arrayValores[2]}/6000" else textPassos.text
 
-        progressBatimentos.progress = (
-                (if (arrayValores[0] > 0) arrayValores[0] else progressBatimentos.progress) * 100
-                ) / 110
+        progressBatimentos.progress =
+            if (arrayValores[0] > 0) (arrayValores[0] * 100) / 110 else progressBatimentos.progress
 
-        progressOxigenacao.progress = (
-                (if (arrayValores[1] > 0) arrayValores[1] else progressOxigenacao.progress) * 100
-                ) / 100
+        progressOxigenacao.progress =
+            if (arrayValores[1] > 0) (arrayValores[1] * 100) / 100 else progressOxigenacao.progress
 
-        progressPassos.progress = (
-                (if (arrayValores[2] > 0) arrayValores[2] else progressPassos.progress) * 100
-                ) / 6000
+        progressPassos.progress =
+            if (arrayValores[2] > 0) (arrayValores[2] * 100) / 6000 else progressPassos.progress
+    }
+
+    fun salvarDados(arrayValores: ArrayList<Int>) {
+        val item = ItemHistorico(
+            Calendar.getInstance().timeInMillis,
+            arrayValores[0],
+            arrayValores[1],
+            (arrayValores[2] * 100) / 6000
+        )
+
+        Persistencia(applicationContext).salvarDados("historico", arrayListOf(item))
     }
 
     override fun onDestroy() {
